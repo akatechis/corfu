7a6e31ef36e422c6fa2d0feac538e12665959a64
Alexandros Katechis <akatechis@gmail.com>
2022-07-11 19:36:30 -0400

    First commit! 🎉

---
+++ mod.ts
@@ -0,0 +1,116 @@
+export type AttrValue =
+  | string
+  | number
+  | boolean
+  | null
+  | undefined;
+
+export type PropValue =
+  | string
+  | number
+  | boolean
+  | null
+  | undefined
+  | ((...args: unknown[]) => unknown);
+
+/**
+ * Attributes represent the attributes of intrinsic elements of HTML, such as div, span, img, etc.
+ */
+export type Attributes = Record<string, AttrValue>;
+export type Props = Record<string, PropValue>;
+export type Template = (props: Props) => string;
+export type PropsWithChildren<T> = T & { children?: string };
+export type IElement = keyof JSX.IntrinsicElements;
+
+export function h(kind: IElement, attrs: Attributes | null, children: string): string;
+export function h(kind: Template, props: Props | null, children: string): string;
+export function h(kind: IElement | Template, props: Props | Attributes | null, children: string): string {
+  if (typeof kind === 'string') {
+    const attrs = props !== null ? ` ${serializeAttrs(props as Attributes)}` : "";
+    if (VoidElementTags[kind]) {
+      return `<${kind}${attrs}>`;
+    }
+    return `<${kind}${attrs}>${children !== undefined ? children : ''}</${kind}>`;
+  }
+  else {
+    const allProps: Props = Object.assign({ ...props }, children ? { children } : {});
+    return kind(allProps);
+  }
+}
+
+function serializeAttrs(attrs: Attributes): string {
+  const items = Object.entries(attrs)
+  .map(([key, val]) => {
+    if (val === null || val === undefined || val === false) {
+      return false;
+    }
+    if (val === true) {
+      return key;
+    }
+    if (typeof val === 'string' || typeof val === 'number') {
+      return `${key}="${val.toString()}"`;
+    }
+  })
+  .filter((item) => item !== false) as string[];
+  return items.join(' ');
+}
+
+/**
+ * Void elements are elements that have no closing tag, such as `<img>`
+ */
+const VoidElementTags: Record<string, boolean> = {
+  area: true,
+  base: true,
+  br: true,
+  col: true,
+  embed: true,
+  hr: true,
+  img: true,
+  input: true,
+  link: true,
+  meta: true,
+  param: true,
+  source: true,
+  track: true,
+  wbr: true,
+};
+
+declare global {
+  namespace JSX {
+    /**
+     * Todo: fill in remaining elements and attrs from https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/react/v17/index.d.ts#L3094
+     */
+    interface IntrinsicElements {
+      div: HTMLDivProps;
+      img: HTMLDivProps;
+      span: HTMLDivProps;
+      script: HTMLDivProps;
+      button: HTMLDivProps;
+      area: true,
+      base: true,
+      br: true,
+      col: true,
+      embed: true,
+      hr: true,
+      link: true,
+      meta: true,
+      param: true,
+      source: true,
+      track: true,
+      wbr: true,
+      input: HTMLInputProps;
+      // will add more later
+    }
+
+    type HTMLInputType = 'checkbox' | 'text';
+
+    interface HTMLInputProps {
+      type: HTMLInputType;
+      checked?: boolean;
+    }
+
+    interface HTMLDivProps {
+      class?: string;
+    }
+  }
+}
---
+++ tests/basic-render.test.tsx
@@ -0,0 +1,42 @@
+/* @jsx h */
+import { h, PropsWithChildren } from "../mod.ts";
+import { expect } from "chai";
+
+Deno.test("can render an intrinsic JSX element", () => {
+  const jsx = <div class="foo">Hello world</div>;
+  expect(jsx).to.equal("<div class=\"foo\">Hello world</div>");
+});
+
+Deno.test("can render an intrinsic JSX element with children", () => {
+  const jsx = <div class="foo"><span>Hello world</span></div>;
+  expect(jsx).to.equal("<div class=\"foo\"><span>Hello world</span></div>");
+});
+
+Deno.test("can render a custom JSX element and remove falsy attrs", () => {
+  interface ButtonProps {
+    primary: boolean;
+  }
+  function Button(props: PropsWithChildren<ButtonProps>): string {
+    const cls = props.primary ? "primary" : "secondary";
+    return <button class={cls}>{props.children}</button>;
+  }
+  const jsx = <Button primary={false}>Click me!</Button>;
+  expect(jsx).to.equal("<button class=\"secondary\">Click me!</button>");
+});
+
+Deno.test("can render a custom JSX element without children", () => {
+  interface ButtonProps {
+    primary: boolean;
+  }
+  function Button(props: PropsWithChildren<ButtonProps>): string {
+    const cls = props.primary ? "primary" : "secondary";
+    return <button class={cls} />;
+  }
+  const jsx = <Button primary={false}>Click me!</Button>;
+  expect(jsx).to.equal("<button class=\"secondary\"></button>");
+});
+
+Deno.test("can render a void element with boolean property", () => {
+  const jsx = <input type="checkbox" checked />;
+  expect(jsx).to.equal("<input type=\"checkbox\" checked>");
+});
